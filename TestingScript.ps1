7a5fb4af0894f8a978aafea17409e28ee0703e64
Toby,Liu <ning.liu4@hp.com>
2015-10-24 16:34:48 +0800

    test

--- TestingScript.ps1
+++ TestingScript.ps1
@@ -3,6 +3,8 @@
 #
 $fileName = "2015-04-22_SA_Incident_IM2314532.txt"
 
+
+
 $output= New-Object psobject
 $output | Add-Member NoteProperty "Date" $fileName.Split("_")[0]
 $output | Add-Member NoteProperty "Application" $fileName.Split("_")[1]
